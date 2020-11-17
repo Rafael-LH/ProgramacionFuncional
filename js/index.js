e950bdf61f72d6d4a72efd516986415c12ec3feb
Rafael Lopez <rafael.iskoat@gmail.com>
2020-11-16 21:21:25 -0600

    Puntando los valores ingresados en el formulario en nuestra UI

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@
   const protein = document.getElementById('protein');
   const containerInputs = [description, calories, carbs, protein];
   let list = []
+  const table = document.getElementById('table')
 
   const errorClass = (element) => element.value ? element.classList.remove('input-error') : element.classList.add('input-error')
   const addErrorsInput = () => containerInputs.forEach(item => errorClass(item))
@@ -19,16 +20,41 @@
     }
     list.push(newProducts)
   }
+
   const formCalories = () => {
     if (description.value && calories.value && carbs.value && protein.value) {
       addProduct()
       cleanInputsSubmit()
+      printTableList()
       console.log(list);
     } else {
       addErrorsInput()
     }
   }
   document.getElementById('btn-add').addEventListener('click', formCalories)
+
+  const printTableList = () => {
+    table.innerHTML = `
+          <tr>
+            <th>Description</th>
+            <th>Calories</th>
+            <th>Carbs</th>
+            <th>Protein</th>
+          </tr>
+        `
+    if (list.length >= 1) {
+      list.forEach(item => {
+        table.innerHTML += `
+        <tr>
+          <td>${item.description}</td>
+          <td>${item.calories}</td>
+          <td>${item.carbs}</td>
+          <td>${item.protein}</td>
+        </tr>
+        `
+      })
+    }
+  }
 
   const removeStyleError = (e) => errorClass(e.target)
   containerInputs.forEach(item => {
